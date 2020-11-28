6d4af8a00c9d7404f83277b30961bbd53895043d
Paisit Jarunnamsiri <paisitj@MacBook-Pro.local>
2020-11-28 11:39:25 -0800

    Section 5: Generic Ties Between Projects

--- mfp/packages/marketing/src/bootstrap.js
+++ mfp/packages/marketing/src/bootstrap.js
@@ -1,12 +1,10 @@
 import React from 'react';
-import ReactDom from 'react-dom';
+import ReactDOM from 'react-dom';
+import App from './App';
 
 // Mount function to start up the app
 const mount = (el) => {
-  ReactDom.render(
-    <h1>Hi there!</h1>,
-    el
-  );
+  ReactDOM.render(<App/>, el);
 };
 
 // If we are in development and in isolation,
